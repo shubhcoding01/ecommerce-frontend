0b30a77b0c8b1187918d5d4a33315566a881e9be
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-25 15:39:52 +0530

    Update package-lock.json and package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 
 function App() {
   return (
-    <div className="">
+    <div className="p-20">
       <h1 className='text-red-500 font-bold text-5xl'>code with shubhcoding</h1>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+      <AddShoppingCartIcon style={{ fontSize: 40, color: 'blue' }} />
     </div>
   );
 }

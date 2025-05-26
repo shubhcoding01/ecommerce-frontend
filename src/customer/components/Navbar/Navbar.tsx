ec29b15bb204caa6670346879c5f15fbef358ad4
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-26 22:20:50 +0530

    Update Navbar component to use new styles

--- src/customer/components/Navbar/Navbar.tsx
+++ src/customer/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, IconButton } from '@mui/material'
+import { Avatar, Box, Button, IconButton } from '@mui/material'
 import React from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
@@ -25,7 +25,8 @@ const Navbar = () => {
                     </IconButton>
                     {
                         true ? <Button>
-                            Bazaar
+                            <Avatar src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2F%40simo_sultan%2Fa-rough-guide-to-create-a-css-avatar-bb9888011887&psig=AOvVaw1xRSffZbsw0GBV7xbUz2Lb&ust=1748364569229000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCID3iN7LwY0DFQAAAAAdAAAAABAE'/>
+                            <h1>Bazaar</h1>
                         </Button> : <Button variant='contained'>Login</Button>
                     }
                 </div>

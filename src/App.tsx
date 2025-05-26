1429304a405227c648142a63d02e2a114ad99392
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-26 21:29:10 +0530

    Updating the App component to include a new feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Navbar from './customer/components/Navbar/Navbar';
 
 
 function App() {
   return (
     <div className="p-20">
       
+      <Navbar />
+
     </div>
   );
 }

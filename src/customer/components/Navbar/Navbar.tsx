985fc91f18edf230d65a681b868d60e25650fcea
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-26 22:25:25 +0530

    Update Navbar component to use new styles

--- src/customer/components/Navbar/Navbar.tsx
+++ src/customer/components/Navbar/Navbar.tsx
@@ -26,7 +26,9 @@ const Navbar = () => {
                     {
                         true ? <Button>
                             <Avatar src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2F%40simo_sultan%2Fa-rough-guide-to-create-a-css-avatar-bb9888011887&psig=AOvVaw1xRSffZbsw0GBV7xbUz2Lb&ust=1748364569229000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCID3iN7LwY0DFQAAAAAdAAAAABAE'/>
-                            <h1>Bazaar</h1>
+                            <h1 className='font-semibold hidden lg:block'>
+                                Bazaar
+                                </h1>
                         </Button> : <Button variant='contained'>Login</Button>
                     }
                 </div>

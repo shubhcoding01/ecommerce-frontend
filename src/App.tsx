9b235e4fdc17c60b4bd972bc4a58d87bea591b18
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-26 21:09:58 +0530

    Updating the App component to include a new feature

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,7 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 function App() {
   return (
     <div className="p-20">
-      <h1 className='text-red-500 font-bold text-5xl'>code with shubhcoding</h1>
-      <Button variant="contained" color="primary">
-        Click Me
-      </Button>
-      <AddShoppingCartIcon style={{ fontSize: 40, color: 'blue' }} />
+      
     </div>
   );
 }

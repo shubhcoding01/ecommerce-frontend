c2f98a534818f3555526efeff73264d3fe00078c
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-26 21:32:45 +0530

    Updating the App component to include a new feature

--- src/customer/components/Navbar/Navbar.tsx
+++ src/customer/components/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
   return (
     <div>
         <Box>
-            
+
         </Box>
     </div>
   )

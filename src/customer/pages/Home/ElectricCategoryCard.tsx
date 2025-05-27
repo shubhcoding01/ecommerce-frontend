c68597c4197eadbea6fd0c01bf0f8696aecc7854
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-27 06:50:30 +0530

    Update ElectricCategory and Home components

--- src/customer/pages/Home/ElectricCategoryCard.tsx
+++ src/customer/pages/Home/ElectricCategoryCard.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ElectricCategoryCard = () => {
   return (
-    <div>ElectricCategoryCard</div>
+    <div>
+        <img className='object-contain h-10'
+        src="https://m.media-amazon.com/images/I/71pKJ+Mjd8L._AC_UY327_FMwebp_QL65_.jpg" alt="" />
+        <h2 className='font-semibold text-sm'>Laptop</h2>
+    </div>
   )
 }
 

45ddbcee7eae68147c17e2036d9ee29271846c07
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-27 07:39:36 +0530

    Update the Navbar and ElectricCategory components

--- src/customer/pages/Home/Home.tsx
+++ src/customer/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ import CategoryGrid from './CategoryGrid/CategoryGrid'
 const Home = () => {
   return (
     <>
-        <div className='space-y-5 lg:space-y-10 relative'>
+        <div className='space-y-5 lg:space-y-10 relative pb-20'>
 
             <ElectricCategory/>
             <CategoryGrid/>

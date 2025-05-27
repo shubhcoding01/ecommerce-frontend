f97ee01b2d37c0087382cf60d4562714c3242b4b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-27 07:23:12 +0530

    Update ElectricCategory and Home components

--- src/customer/pages/Home/CategoryGrid/CategoryGrid.tsx
+++ src/customer/pages/Home/CategoryGrid/CategoryGrid.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const CategoryGrid = () => {
   return (
-    <div>
+    <div className='grid gap-4 grid-rows-12 grid-cols-12 lg:h-[600px] px-5 lg:px-20'>
+        
+        <div className='col-span-3 row-span-12 text-white'>
+            <img 
+            src="https://m.media-amazon.com/images/I/71jF7678loL._SX569_.jpg" alt="" />
+        </div>
+        <div className='col-span-2 row-span-6 text-white'>
+            <img 
+            src="https://m.media-amazon.com/images/I/61hYT677lhL._AC_UL960_FMwebp_QL65_.jpg" alt="" />
+        </div>
         
     </div>
   )
